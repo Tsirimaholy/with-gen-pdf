6bedf577b4da8947bcac238521d183eee6f2d3b3
tsirimaholy <hei.tsirimaholy@gmail.com>
2023-08-27 14:56:01 +0300

    fix: remove unecessary birth date checking

--- src/main/java/com/example/withth/service/utils/DateUtils.java
+++ src/main/java/com/example/withth/service/utils/DateUtils.java
@@ -5,19 +5,13 @@ import java.time.Period;
 
 public class DateUtils {
     public static int calculateAgeAtExactBirthday(LocalDate birthdate, LocalDate currentDate) {
-        if (birthdate != null && currentDate != null) {
-            if (
-                    currentDate.getMonth().getValue() > birthdate.getMonth().getValue() ||
-                    (
-                            currentDate.getMonth().getValue() == birthdate.getMonth().getValue() &&
-                            currentDate.getDayOfMonth() >= birthdate.getDayOfMonth()
-                    )
-            ) {
-                return Period.between(birthdate, currentDate).getYears();
-            } else {
-                return Period.between(birthdate.minusYears(1), currentDate).getYears();
-            }
+        if (currentDate == null) {
+            currentDate = LocalDate.now();
         }
-        return 0;
+        if (birthdate == null) {
+            throw new IllegalArgumentException("Birth date cannot be null");
+        }
+
+        return Period.between(birthdate, currentDate).getYears();
     }
 }
